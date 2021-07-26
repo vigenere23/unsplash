96b1d38e8a68e268a13037e0a8f4d20acb2c0c06
Gab S <32545895+vigenere23@users.noreply.github.com>
2021-07-25 21:59:08 -0400

    Support for Linux (#2)
    * added linux installation script
    
    * created linux commands
    
    * created separated requirements.txt files
    
    * fixed dependencies

--- src/commands/set/set_command_linux.py
+++ src/commands/set/set_command_linux.py
@@ -6,11 +6,9 @@ from commands.config.config_repository import ConfigRepository
 from commands.set.set_command import SetCommand
 
 
-class SetCommandMacOS(SetCommand):
+class SetCommandLinux(SetCommand):
     def __init__(self, config_repo: ConfigRepository):
         super().__init__(path.join(Path.home(), 'Pictures', 'unsplash', 'current'), config_repo)
 
     def _set_new_wallpaper(self, image_path: str):
-        app('Finder').desktop_picture.set(mactypes.File(image_path))
-        subprocess.call(['osascript', '-e', f'tell application "Finder" to set desktop picture to POSIX file "{image_path}"'])
-        subprocess.call(['/usr/bin/killall', 'Dock'])
+        subprocess.call(['/usr/bin/gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', f'file://{image_path}'])
